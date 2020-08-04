3e7dce243d54c58a02a51533f74075d12c67ccdb
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2020-08-04 21:59:09 +0300

    update test

--- src/test/java/guru/sfg/brewery/web/controllers/ControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/ControllerIT.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -32,7 +33,7 @@ public class ControllerIT extends BaseIT {
 
     @Test
     void findBeersPermitAll() throws Exception {
-        mockMvc.perform(get("/beers/find"))
+        mockMvc.perform(get("/beers/find").with(anonymous()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
