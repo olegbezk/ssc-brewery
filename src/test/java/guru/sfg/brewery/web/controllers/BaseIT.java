9e42d9694e6134ea6cca69cbb84fd3d0c3ccf43f
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2020-08-04 21:32:48 +0300

    initial security config

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
@@ -0,0 +1,41 @@
+package guru.sfg.brewery.web.controllers;
+
+import guru.sfg.brewery.repositories.BeerInventoryRepository;
+import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.services.BeerService;
+import guru.sfg.brewery.services.BreweryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+public class BaseIT {
+    @Autowired
+    protected WebApplicationContext wac;
+
+    protected MockMvc mockMvc;
+
+    @MockBean
+    protected BeerRepository beerRepository;
+    @MockBean
+    protected BeerInventoryRepository beerInventoryRepository;
+    @MockBean
+    protected BreweryService breweryService;
+    @MockBean
+    protected CustomerRepository customerRepository;
+    @MockBean
+    protected BeerService beerService;
+
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
+}
---
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
@@ -0,0 +1,23 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class IndexControllerIT extends BaseIT {
+
+    @Test
+    void testGetIndexSlash() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetLogin() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk());
+    }
+}
