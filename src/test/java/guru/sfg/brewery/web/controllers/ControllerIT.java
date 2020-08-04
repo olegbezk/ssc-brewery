1583b648d8f2eb872561c55bab970697c3e1f622
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2020-08-04 21:51:59 +0300

    refactor

--- src/test/java/guru/sfg/brewery/web/controllers/ControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/ControllerIT.java
@@ -11,7 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest
-public class BeerControllerIT extends BaseIT {
+public class ControllerIT extends BaseIT {
 
     @WithMockUser()
     @Test
