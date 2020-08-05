10a0c8e61620b280017baaded743fa69330025a0
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2020-08-05 14:56:28 +0300

    user details service

--- src/test/java/guru/sfg/brewery/web/controllers/ControllersIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/ControllersIT.java
@@ -12,6 +12,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class ControllersIT extends BaseIT {
 
+    @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
     @WithMockUser()
     @Test
     void findBeers() throws Exception {
