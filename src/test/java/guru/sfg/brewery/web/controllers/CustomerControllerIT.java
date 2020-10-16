a9f76bcd6290865f149c4bda33fa1846b2e50b6c
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2020-10-16 21:45:39 +0300

    fix tests

--- src/test/java/guru/sfg/brewery/web/controllers/CustomerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/CustomerControllerIT.java
@@ -7,16 +7,17 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.test.annotation.Rollback;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 
 public class CustomerControllerIT extends BaseIT {
 
     @DisplayName("List Customers")
     @Nested
-    class ListCustomers{
+    class ListCustomers {
         @ParameterizedTest(name = "#{index} with [{arguments}]")
         @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamAdminCustomer")
         void testListCustomersAUTH(String user, String pwd) throws Exception {
@@ -47,8 +48,9 @@ public class CustomerControllerIT extends BaseIT {
 
         @Rollback
         @Test
-        void processCreationForm() throws Exception{
+        void processCreationForm() throws Exception {
             mockMvc.perform(post("/customers/new")
+                    .with(csrf())
                     .param("customerName", "Foo Customer")
                     .with(httpBasic("admin", "admin")))
                     .andExpect(status().is3xxRedirection());
@@ -57,7 +59,7 @@ public class CustomerControllerIT extends BaseIT {
         @Rollback
         @ParameterizedTest(name = "#{index} with [{arguments}]")
         @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamNotAdmin")
-        void processCreationFormNOTAUTH(String user, String pwd) throws Exception{
+        void processCreationFormNOTAUTH(String user, String pwd) throws Exception {
             mockMvc.perform(post("/customers/new")
                     .param("customerName", "Foo Customer2")
                     .with(httpBasic(user, pwd)))
@@ -65,8 +67,9 @@ public class CustomerControllerIT extends BaseIT {
         }
 
         @Test
-        void processCreationFormNOAUTH() throws Exception{
+        void processCreationFormNOAUTH() throws Exception {
             mockMvc.perform(post("/customers/new")
+                    .with(csrf())
                     .param("customerName", "Foo Customer"))
                     .andExpect(status().isUnauthorized());
         }
