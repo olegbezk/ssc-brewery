f785122eb4fbd2a503a49b2d12061e918a050f1a
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2020-10-18 07:45:26 +0300

    add login failure persistence

---
+++ src/main/java/guru/sfg/brewery/domain/security/LoginFailure.java
@@ -0,0 +1,44 @@
+package guru.sfg.brewery.domain.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class LoginFailure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    private String userName;
+
+    private String sourceIp;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+}
--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureBadCredentialsListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureBadCredentialsListener.java
@@ -1,5 +1,9 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.LoginFailure;
+import guru.sfg.brewery.repositories.security.LoginFailureRepository;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,24 +13,46 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class AuthenticationFailureBadCredentialsListener {
+
+    private final LoginFailureRepository loginFailureRepository;
+    private final UserRepository userRepository;
 
     @EventListener
     public void listen(AuthenticationFailureBadCredentialsEvent event) {
         log.debug("User With Bad Credentials.");
 
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+
+            LoginFailure.LoginFailureBuilder builder = LoginFailure.builder();
+
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
 
             if (token.getPrincipal() instanceof String) {
-                log.debug("Attempted Username: " + token.getPrincipal());
+                Object principal = token.getPrincipal();
+
+                log.debug("Attempted Username: " + principal);
+
+                builder.userName((String) principal);
+
+                userRepository.findByUsername((String) principal).ifPresent(builder::user);
             }
 
             if (token.getDetails() instanceof WebAuthenticationDetails) {
+
                 WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
 
-                log.debug("Source IP: " + details.getRemoteAddress());
+                String remoteAddress = details.getRemoteAddress();
+
+                builder.sourceIp(remoteAddress);
+
+                log.debug("Source IP: " + remoteAddress);
             }
+
+            LoginFailure loginFailure = loginFailureRepository.save(builder.build());
+
+            log.debug("Login failure user ID: " + loginFailure.getId());
         }
     }
 }
