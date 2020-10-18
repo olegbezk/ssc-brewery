a9f91457416f4c9eb2a426987f17ec437c187906
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2020-10-18 12:07:49 +0300

    add account locking

--- src/main/java/guru/sfg/brewery/repositories/security/LoginFailureRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/LoginFailureRepository.java
@@ -1,9 +1,15 @@
 package guru.sfg.brewery.repositories.security;
 
 import guru.sfg.brewery.domain.security.LoginFailure;
+import guru.sfg.brewery.domain.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 @Repository
 public interface LoginFailureRepository extends JpaRepository<LoginFailure, Long> {
+
+    List<LoginFailure> findAllByUserAndCreatedDateIsAfter(User user, Timestamp timestamp);
 }
--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureBadCredentialsListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureBadCredentialsListener.java
@@ -1,6 +1,7 @@
 package guru.sfg.brewery.security.listeners;
 
 import guru.sfg.brewery.domain.security.LoginFailure;
+import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.LoginFailureRepository;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -53,6 +58,21 @@ public class AuthenticationFailureBadCredentialsListener {
             LoginFailure loginFailure = loginFailureRepository.save(builder.build());
 
             log.debug("Login failure user ID: " + loginFailure.getId());
+
+            if (loginFailure.getUser() != null) {
+                lockUserAccount(loginFailure.getUser());
+            }
+        }
+    }
+
+    private void lockUserAccount(User user) {
+        List<LoginFailure> allByUserAndCreatedDateIsAfter = loginFailureRepository.findAllByUserAndCreatedDateIsAfter(
+                user, Timestamp.valueOf(LocalDateTime.now().minusDays(1)));
+
+        if (allByUserAndCreatedDateIsAfter.size() > 3) {
+            log.debug("Locking User Account...");
+            user.setAccountNonLocked(false);
+            userRepository.save(user);
         }
     }
 }
